42f4d7bebb33d29789329af588f3515da34cd51a
BilgeBengisu <76906763+BilgeBengisu@users.noreply.github.com>
2025-09-26 11:51:14 -0400

    observation space turned into a dict

--- OurGridWorld/GridWorld/gymnasium_env/envs/__init__.py
+++ OurGridWorld/GridWorld/gymnasium_env/envs/__init__.py
@@ -1,4 +1,2 @@
 from gymnasium_env.envs.grid_world import GridWorldEnv
-from gymnasium_env.envs.grid_world_no_target import GridWorldNoTargetEnv
-
-__all__ = ["GridWorldEnv", "GridWorldNoTargetEnv"]
+__all__ = ["GridWorldEnv"]
